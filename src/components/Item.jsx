e83e26806860b4f779a5551f9b93dc2769a7e882
Islam Kamal <islamkamalx1>
2023-11-14 20:30:12 +0200

    shopping list

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -11,7 +11,7 @@ import { useNavigate } from "react-router-dom";
 const Item = ({ item, width }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(1);
   const [isHovered, setIsHovered] = useState(false);
   const {
     palette: { neutral },
--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -1,9 +1,11 @@
 import MainCarousel from "./MainCarousel.jsx";
+import ShoppingList from "./ShoppingList.jsx";
 
 const Home = () => {
   return (
     <div className="home">
       <MainCarousel />
+      <ShoppingList />
     </div>
   );
 };
